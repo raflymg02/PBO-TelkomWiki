a650088546ce0b7a88f73ec52ee4a79bb308d72c
AhmadND00 <ahmadnd00@protonmail.com>
2023-11-06 13:08:37 +0700

    Change folder structure

--- src/main/java/telkomwiki/TelkomWiki.java
+++ src/main/java/telkomwiki/TelkomWiki.java
@@ -1,22 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package telkomwiki;
 
 /**
  *
- * @author ASUS
+ * @author Kelompok 1
  */
 public class TelkomWiki {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello");
+    public static void main(String[] args) throws Exception {
+
+
+        // Local Database
+        // String sql = "SELECT first_name, last_name FROM mock_data WHERE id=8";
+
+        // String url = "jdbc:postgresql://localhost:5432/pbo";
+        // String username = "demo";
+        // String password = "1234";
+
+        // Connection con = DriverManager.getConnection(url, username, password);
+        // Statement st = con.createStatement();
+        // ResultSet rs = st.executeQuery(sql);
+
+        // if (rs.next()) {
+        //     String firstName = rs.getString("first_name");
+        //     String lastName = rs.getString("last_name");
+
+        //     System.out.println("First Name: " + firstName);
+        //     System.out.println("Last Name: " + lastName);
+        // } else {
+        //     System.out.println("No data found for ID 8.");
+        // }
+
+        // con.close();
+
     }
 
 }
