2c2093d1adf4b4434f54702e1c220a5524121f0f
Farrel Muhammad Al-Falah <122030058+Farrelmflh@users.noreply.github.com>
2023-11-07 07:26:51 +0700

    Implement Local Database

---
+++ src/main/java/telkomwiki/Tagged.java
@@ -0,0 +1,25 @@
+package telkomwiki;
+
+public class Tagged {
+    private WikiPage[] pages;
+    private Tag[] tags;
+    public Tagged(WikiPage[] pages, Tag[] tags) {
+        this.pages = pages;
+        this.tags = tags;
+    }
+    public WikiPage[] getPages(){
+        return pages;
+    }
+    public void setPages(WikiPage[] pages) {
+        this.pages = pages;
+    }
+    public Tag[] getTags() {
+        return tags;
+    }
+    public void setTags(Tag[] tags) {
+        this.tags = tags;
+    }
+    public String toString() {
+        return "Tagged{" +"pages=" + Arrays.toString(pages) +", tags=" + Arrays.toString(tags) +'}';
+    }
+}
