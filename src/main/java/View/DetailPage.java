7ec93746dcd3e67472d2a6c9fe84681ea1cc67a2
Fakhri F <fakhri6203@gmail.com>
2024-01-04 21:24:15 +0700

    Connect DB to UI

--- src/main/java/View/DetailPage.java
+++ src/main/java/View/DetailPage.java
@@ -10,42 +10,51 @@ import javax.swing.JTextArea;
 
 public class DetailPage extends JFrame {
 
-    DetailPage(String Judul, String Matkul) {
+    DetailPage(String Judul, String Matkul, String Deskripsi, String tag) {
         setTitle(Judul + " - " + Matkul);
-        setSize(800, 600);
-        add(thisPanel(Judul, Matkul));
+        setSize(800, 800);
+        add(thisPanel(Judul, Matkul, Deskripsi, tag));
         setVisible(true);
     }
 
-    JPanel thisPanel(String Judul, String Matkul) {
+    JPanel thisPanel(String Judul, String Matkul, String Deskripsi, String tag) {
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(Color.WHITE);
 
         JLabel topicTitle = new JLabel();
+        JLabel tagTitle = new JLabel();
         JLabel matkulTitle = new JLabel();
         JTextArea deskripsi = new JTextArea();
 
         topicTitle.setText(Judul);
         topicTitle.setFont(new Font("Calibri", Font.BOLD, 30));
-        topicTitle.setSize(300, 30);
+        topicTitle.setSize(500, 30);
         topicTitle.setLocation(20, 30);
 
         matkulTitle.setText("Mata Kuliah: " + Matkul);
-        matkulTitle.setSize(300, 30);
+        matkulTitle.setSize(300, 20);
         matkulTitle.setLocation(20, 60);
 
-        deskripsi.setText(
-                "Ini adalah text yang panjang, sangking panjangnya aku mager ngisi. Dah lah ya ini mah harusnya tuh Lorem Ipsum blabla gitu. Tapi karena ga hafal aku ubah aja jadi gini. Dah ya, disini baris kodenya panjang.");
+        tagTitle.setText("Tag: " + tag);
+        tagTitle.setSize(300, 20);
+        tagTitle.setLocation(20, 80);
+
+        // deskripsi.setText(
+        // "Ini adalah text yang panjang, sangking panjangnya aku mager ngisi. Dah lah
+        // ya ini mah harusnya tuh Lorem Ipsum blabla gitu. Tapi karena ga hafal aku
+        // ubah aja jadi gini. Dah ya, disini baris kodenya panjang.");
+        deskripsi.setText(Deskripsi);
         deskripsi.setLineWrap(true);
         deskripsi.setOpaque(false);
         deskripsi.setEditable(false);
         deskripsi.setFocusable(false);
-        deskripsi.setSize(700, 600);
-        deskripsi.setLocation(20, 100);
+        deskripsi.setSize(750, 600);
+        deskripsi.setLocation(20, 120);
 
         panel.add(topicTitle);
         panel.add(matkulTitle);
+        panel.add(tagTitle);
         panel.add(deskripsi);
 
         return panel;
